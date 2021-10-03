53ef49f8b1dda264fdbe7565d46c58c1060b201c
Prince Yekpahi <princeyekpahi@hotmail.fr>
2021-10-04 00:31:01 +0200

    commit 04 10 2021

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,17 @@
 import { AggregationCursor } from "mongoose";
 import User from "../models/user";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import { hashPassword, comparePassword } from "../utils/auth";
+import AWS from "aws-sdk";
+
+const awsConfig = {
+  accessKeyID: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  apiVersion: process.env.AWS_API_VERSION,
+};
+
+const SES = new AWS.SES(awsConfig);
 
 export const register = async (req, res) => {
   try {
@@ -21,13 +31,13 @@ export const register = async (req, res) => {
 
     //register
     const user = new User({
-        name, 
-        email,
-        password: hashedPassword,
+      name,
+      email,
+      password: hashedPassword,
     });
     await user.save();
     console.log("Saved user", user);
-    return res.json({ ok : true})
+    return res.json({ ok: true });
   } catch (err) {
     console.log(err);
     return res.status(400).send("Error, Try again.");
@@ -36,37 +46,37 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   try {
-    const { email, password} = req.body
+    const { email, password } = req.body;
     //check if our db has user with that email
     const user = await User.findOne({ email }).exec();
     if (!user) return res.status(400).send("No user found");
     //Check password
     const match = await comparePassword(password, user.password);
     //create signed jwt
-    const token = jwt.sign({_id:user._id}, process.env.JWT_SECRET, {
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
     //return user and token to client, exclude hashed password
     user.password = undefined;
     //send token in cookie
     res.cookie("token", token, {
-      httpOnly: true
+      httpOnly: true,
       //secure: true //only works on https
     });
     // send user as json response
-    res.json(user)
+    res.json(user);
   } catch (err) {
-    console.log(err)
-    return res.status(400).send("Erro, Try again.")
+    console.log(err);
+    return res.status(400).send("Erro, Try again.");
   }
 };
 
-export const logout = async(req, res) => {
+export const logout = async (req, res) => {
   try {
     res.clearCookie("token");
-    return res.json({message : "Signout success"});
+    return res.json({ message: "Signout success" });
   } catch (errr) {
-    console.log(err)
+    console.log(err);
   }
 };
 
@@ -74,13 +84,46 @@ export const currentUser = async (req, res) => {
   try {
     const user = await User.findById(req.user._id).select("-password").exec();
     console.log("CURRENT_USER", user);
-    return res.json({ ok : true});
+    return res.json({ ok: true });
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 };
 
 export const sendTestEmail = async (req, res) => {
-  console.log("Send email is using ses")
-  res.json({ok : true})
+  //console.log("Send email is using ses")
+  //res.json({ok : true})
+  const params = {
+    Source: process.env.EMAIL_FROM,
+    Destination: {
+      ToAddresses: ["skyleyoro01@gmail.com"],
+    },
+  
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: `<html>
+            <h1> Reset password Link</h1>
+            <p>Please use the following link to reset your passsword</p>
+          </html>`,
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Passord reset link",
+      },
+    },
+  };
+
+  const emailSent = SES.sendEmail(params).promise();
+
+  emailSent
+    .then((data) => {
+      console.log(data);
+      res.json({ ok: true });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
